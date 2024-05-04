3dbe45b26d40f05d1366a063218775b55e7ad9ca
JuanPaJP123 <139984266+JpMg123@users.noreply.github.com>
2024-05-03 21:29:12 -0600

    Configurando Link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,14 @@
 import '../styles/Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <div>
       <div className="navbar">
-        <a href="/">Inicio</a>
-        <a href="/productos">Productos</a>
-        <a href="/contacto">Contacto</a>
-        <a href="/otros">Otros</a>
+        <Link to="/">Inicio</Link>
+        <Link  to="/productos">Productos</Link>
+        <Link to="/contacto">Contacto</Link>
+        <Link to="/otros">Otros</Link>
       </div>
     </div>
   );
