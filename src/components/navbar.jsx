0e5212b6e616d8b63e16709bf2b286e104e4666a
JuanPaJP123 <139984266+JpMg123@users.noreply.github.com>
2024-05-03 21:18:57 -0600

    Reparando Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar() {
     <div>
       <div className="navbar">
         <a href="/">Inicio</a>
-        <a href="/Productos">Productos</a>
-        <a href="/Contacto">Contacto</a>
-        <a href="/Otros">Otros</a>
+        <a href="/productos">Productos</a>
+        <a href="/contacto">Contacto</a>
+        <a href="/otros">Otros</a>
       </div>
     </div>
   );
